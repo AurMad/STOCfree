57b3e8c47dcc3412a7aa56b5cd0f8c545ee94ce6
Aurélien Madouasse <aurelien.madouasse@oniris-nantes.fr>
2019-12-18 11:24:17 +0100

    Functions to run the model and present the results have been added.

---
+++ R/sample_model.R
@@ -0,0 +1,69 @@
+#' Draws samples from the posterior distributions of model parameters
+#'
+#' @param compiled_model a model compiled by compile_JAGS()
+#' @param n_burnin number of burnin iterations
+#' @param n_iter number of iterations to monitor
+#' @param n_thin thinning interval for monitors
+#'
+#' @return a list with 2 variables
+#' @export
+#'
+#' @examples
+sample_model <- function(compiled_model, n_burnin, n_iter, n_thin){
+
+  ## iterations for burnin
+  update(compiled_model, n.iter = n_iter)
+
+  ## samples from posterior distributions
+  variables_to_save <- c("Se", "Sp", "theta", "tau2",
+                         "predicted_proba", "predicted_status")
+
+  samples <- rjags::coda.samples(compiled_model,
+                          variable.names = variables_to_save,
+                          n.iter = n_iter,
+                          thin = n_thin)
+
+  ## tidying the results for predcited probabilities
+  predictions <- tidybayes::spread_draws(samples,
+                              predicted_proba[herd_id],
+                              predicted_status[herd_id])
+
+  ## tidying the results for model parameters
+  parameters <- tidybayes::spread_draws(samples, Se, Sp, tau2, theta[..])
+
+  ## Percentiles of predicted probabilities of infection according to predicted status
+  choice_cutoff <- tibble::tibble(
+    cutoff = seq(0, 1, by = .01),
+    pred_Se = rep(NA),
+    pred_Sp = rep(NA),
+    pred_ppv = rep(NA),
+    pred_npv = rep(NA)
+  )
+
+  for(i in 1:nrow(choice_cutoff)){
+
+    cutoff <- choice_cutoff$cutoff[i]
+
+    true_pos <- nrow(predictions[predictions$predicted_status == 1 &
+                                   predictions$predicted_proba > cutoff,])
+    true_neg <- nrow(predictions[predictions$predicted_status == 0 &
+                                   predictions$predicted_proba <= cutoff,])
+    false_pos <- nrow(predictions[predictions$predicted_status == 0 &
+                                    predictions$predicted_proba > cutoff,])
+    false_neg <- nrow(predictions[predictions$predicted_status == 1 &
+                                    predictions$predicted_proba <= cutoff,])
+
+    choice_cutoff$pred_Se[i] <- true_pos / (true_pos + false_neg)
+    choice_cutoff$pred_Sp[i] <- true_neg / (true_neg + false_pos)
+    choice_cutoff$pred_ppv[i] <- true_pos / (true_pos + false_pos)
+    choice_cutoff$pred_npv[i] <- true_neg / (true_neg + false_neg)
+
+  }
+
+  list(
+    parameters = parameters,
+    proba_inf = predictions,
+    choice_cutoff = choice_cutoff
+  )
+
+}
--- R/write_JAGS_model.R
+++ R/write_JAGS_model.R
@@ -42,14 +42,14 @@ cat('model{
     pi[ind_p[ind_last_is_test[j]]] <- tau1[ind_p[ind_last_is_test[j]]] * (1 - Status[ind_f[ind_last_is_test[j]]]) +
       tau2 * Status[ind_f[ind_last_is_test[j]]]
 
-    piPred[ind_last_is_test[j]] <- test_res[ind_p[ind_last_is_test[j]]] *
+    predicted_proba[ind_last_is_test[j]] <- test_res[ind_p[ind_last_is_test[j]]] *
       (Se * pi[ind_p[ind_last_is_test[j]]]) /
       (Se * pi[ind_p[ind_last_is_test[j]]] + (1 - Sp) * (1 - pi[ind_p[ind_last_is_test[j]]])) +
       (1 - test_res[ind_p[ind_last_is_test[j]]]) *
       (1 - Se) * pi[ind_p[ind_last_is_test[j]]] /
       ((1 - Se) * pi[ind_p[ind_last_is_test[j]]] + Sp * (1 - pi[ind_p[ind_last_is_test[j]]]))
 
-    StatusPred[ind_last_is_test[j]] ~ dbern(piPred[ind_last_is_test[j]])
+    predicted_status[ind_last_is_test[j]] ~ dbern(predicted_proba[ind_last_is_test[j]])
 
   }
 
@@ -57,10 +57,10 @@ cat('model{
   ## Loop for statuses to predict when test result is not available
   for(k in 1:n_pred_no_test){
 
-    piPred[ind_last_is_not_test[k]] <- tau1[ind_p[ind_last_is_not_test[k]]] * (1 - Status[ind_f[ind_last_is_not_test[k]]]) +
+    predicted_proba[ind_last_is_not_test[k]] <- tau1[ind_p[ind_last_is_not_test[k]]] * (1 - Status[ind_f[ind_last_is_not_test[k]]]) +
       tau2 * Status[ind_f[ind_last_is_not_test[k]]]
 
-    StatusPred[ind_last_is_not_test[k]] ~ dbern(piPred[ind_last_is_not_test[k]])
+    predicted_status[ind_last_is_not_test[k]] ~ dbern(predicted_proba[ind_last_is_not_test[k]])
 
   }
 
