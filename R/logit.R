88337648799a0518104017aa31c99240f57f8c6a
Aurélien Madouasse <aurelien.madouasse@oniris-nantes.fr>
2020-01-06 11:58:35 +0100

    logit and inverse logit functions added

---
+++ R/logit.R
@@ -0,0 +1,28 @@
+#' logit function
+#'
+#' @param p a proportion
+#'
+#' @return the logit of a proportion
+#' @export
+#'
+#' @examples
+logit <- function(p){
+
+  if(p < 0 | p > 1) stop("p should be between 0 and 1")
+
+  log(p / (1 - p))
+
+}
+
+#' Inverse logit function
+#'
+#' @param x a number
+#'
+#' @return a proportion
+#' @export
+#'
+#' @examples
+invlogit <- function(x){
+
+  exp(x) / (1 + exp(x))
+}
