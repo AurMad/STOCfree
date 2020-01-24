1591724717220be0ec229ffaf5a9c6a7d7f772b9
Aurélien Madouasse <aurelien.madouasse@oniris-nantes.fr>
2020-01-24 14:29:47 +0100

    Code working for 1 or several tests and risk factors

--- R/sample_model.R
+++ R/sample_model.R
@@ -14,6 +14,31 @@ sample_model <- function(compiled_model,
                          n_iter,
                          n_thin){
 
+  UseMethod("sample_model")
+
+  }
+
+
+
+#' @export
+sample_model.default <- function(compiled_model,
+                                 n_burnin,
+                                 n_iter,
+                                 n_thin){
+
+  print("default")
+
+}
+
+
+#' @export
+sample_model.herd_1test_rf <- function(compiled_model,
+                                        n_burnin,
+                                        n_iter,
+                                        n_thin){
+
+  class(compiled_model) <- "jags"
+
   ## iterations for burnin
   update(compiled_model, n.iter = n_iter)
 
@@ -22,20 +47,20 @@ sample_model <- function(compiled_model,
                          "predicted_proba", "predicted_status")
 
   samples <- rjags::coda.samples(compiled_model,
-                          variable.names = variables_to_save,
-                          n.iter = n_iter,
-                          thin = n_thin)
+                                 variable.names = variables_to_save,
+                                 n.iter = n_iter,
+                                 thin = n_thin)
 
   ## tidying the results for predcited probabilities
   predictions <- tidybayes::spread_draws(samples,
-                              predicted_proba[herd_id],
-                              predicted_status[herd_id])
+                                         predicted_proba[herd_id],
+                                         predicted_status[herd_id])
 
   ## tidying the results for model parameters
-  parameters <- tidybayes::spread_draws(samples, Se[..], Sp[..], tau2, theta[..])
+  parameters <- tidybayes::spread_draws(samples, Se, Sp, tau2, theta[..])
 
   ## Percentiles of predicted probabilities of infection according to predicted status
-  choice_cutoff <- tibble::tibble(
+  choice_cutoff <- data.frame(
     cutoff = seq(0, 1, by = .01),
     pred_Se = rep(NA),
     pred_Sp = rep(NA),
@@ -73,3 +98,74 @@ sample_model <- function(compiled_model,
   )
 
 }
+
+
+#' @export
+sample_model.herd_ntests_rf <- function(compiled_model,
+                                        n_burnin,
+                                        n_iter,
+                                        n_thin){
+
+  class(compiled_model) <- "jags"
+
+  ## iterations for burnin
+  update(compiled_model, n.iter = n_iter)
+
+  ## samples from posterior distributions
+  variables_to_save <- c("Se", "Sp", "theta", "tau2",
+                         "predicted_proba", "predicted_status")
+
+  samples <- rjags::coda.samples(compiled_model,
+                          variable.names = variables_to_save,
+                          n.iter = n_iter,
+                          thin = n_thin)
+
+  ## tidying the results for predcited probabilities
+  predictions <- tidybayes::spread_draws(samples,
+                              predicted_proba[herd_id],
+                              predicted_status[herd_id])
+
+  ## tidying the results for model parameters
+  parameters <- tidybayes::spread_draws(samples, Se[..], Sp[..], tau2, theta[..])
+
+  ## Percentiles of predicted probabilities of infection according to predicted status
+  choice_cutoff <- data.frame(
+    cutoff = seq(0, 1, by = .01),
+    pred_Se = rep(NA),
+    pred_Sp = rep(NA),
+    pred_ppv = rep(NA),
+    pred_npv = rep(NA)
+  )
+
+  for(i in 1:nrow(choice_cutoff)){
+
+    cutoff <- choice_cutoff$cutoff[i]
+
+    true_pos <- nrow(predictions[predictions$predicted_status == 1 &
+                                   predictions$predicted_proba > cutoff,])
+    true_neg <- nrow(predictions[predictions$predicted_status == 0 &
+                                   predictions$predicted_proba <= cutoff,])
+    false_pos <- nrow(predictions[predictions$predicted_status == 0 &
+                                    predictions$predicted_proba > cutoff,])
+    false_neg <- nrow(predictions[predictions$predicted_status == 1 &
+                                    predictions$predicted_proba <= cutoff,])
+
+    choice_cutoff$pred_Se[i] <- true_pos / (true_pos + false_neg)
+    choice_cutoff$pred_Sp[i] <- true_neg / (true_neg + false_pos)
+    choice_cutoff$pred_ppv[i] <- true_pos / (true_pos + false_pos)
+    choice_cutoff$pred_npv[i] <- true_neg / (true_neg + false_neg)
+
+  }
+
+  choice_cutoff$pred_ppv[is.nan(choice_cutoff$pred_npv)] <- choice_cutoff$pred_Se[is.nan(choice_cutoff$pred_npv)]
+  choice_cutoff$pred_ppv[is.nan(choice_cutoff$pred_ppv)] <- choice_cutoff$pred_Sp[is.nan(choice_cutoff$pred_ppv)]
+
+  list(
+    parameters = parameters,
+    proba_inf = predictions,
+    choice_cutoff = choice_cutoff
+  )
+
+}
+
+
--- R/write_JAGS_model.R
+++ R/write_JAGS_model.R
@@ -130,3 +130,111 @@ cat('model{
 }',
       file =  "JAGS_model.txt")
 }
+
+#' @export
+write_JAGS_model.herd_1test_rf <- function(data){
+
+  cat('model{
+
+##############################################################################
+###  Inference from historical data
+##############################################################################
+
+  ## Loop for infection dynamics up to test before last
+  for(h in 1:n_herds){
+
+    # First month
+    pi[ind_i[h]] ~ dbeta(pi1_beta_a, pi1_beta_b)
+    Status[ind_i[h]] ~ dbern(pi[ind_i[h]])
+
+    # Months 2 to 1 minus final
+    for(t in ind_j[h]:ind_f[h]){
+
+      logit(tau1[t]) <- inprod(risk_factors[t,], theta)
+
+      pi[t] <- tau1[t] * (1 - Status[t-1]) +
+               tau2 * Status[t-1]
+
+      Status[t] ~ dbern(pi[t])
+
+    } #t
+
+    ## tau1 for test to predict
+    logit(tau1[ind_p[h]]) <- inprod(risk_factors[ind_p[h],], theta)
+
+  }
+
+
+  ## Loop for test results - historical data
+  for(i in 1:n_tests_perf){
+
+    pTestPos[i] <-  Se * Status[ind_test[i]] +
+                    (1 - Sp) * (1 - Status[ind_test[i]])
+
+    test_res[i] ~ dbern(pTestPos[i])
+
+  }
+
+
+##############################################################################
+###  Prediction of probability of infection
+##############################################################################
+
+  ## Loop for statuses to predict when test result is available
+  for(j in 1:n_pred_test){
+
+    ## Indices in loops:
+    # ind_p_test: relates to status index
+    # ind_last_is_test relates to herd_id -> 1 to number of herds between this loop and the next
+    # test_for_pred relates to the cases when test is available for prediction -> 1 to number of such cases
+
+    # After having observed the risk factors and the test result
+    pi[ind_p_test[j]] <- tau1[ind_p_test[j]] * (1 - Status[ind_p_test[j] - 1]) +
+                               tau2 * Status[ind_p_test[j] - 1]
+
+    predicted_proba[ind_last_is_test[j]] <-
+    test_for_pred[j] *
+      (Se * pi[ind_p_test[j]]) /
+      (Se * pi[ind_p_test[j]] + (1 - Sp) * (1 - pi[ind_p_test[j]])) +
+      (1 - test_for_pred[j]) *
+      (1 - Se) * pi[ind_p_test[j]] /
+      ((1 - Se) * pi[ind_p_test[j]] + Sp * (1 - pi[ind_p_test[j]]))
+
+    predicted_status[ind_last_is_test[j]] ~ dbern(predicted_proba[ind_last_is_test[j]])
+
+  }
+
+
+  ## Loop for statuses to predict when test result is not available
+  for(k in 1:n_pred_no_test){
+
+    predicted_proba[ind_last_is_not_test[k]] <- tau1[ind_p_no_test[k]] * (1 - Status[ind_p_no_test[k] - 1]) +
+                                                tau2 * Status[ind_f[ind_last_is_not_test[k]]]
+
+    predicted_status[ind_last_is_not_test[k]] ~ dbern(predicted_proba[ind_last_is_not_test[k]])
+
+  }
+
+
+##############################################################################
+###  Priors
+##############################################################################
+
+  ## Priors for sensitivities and specificities
+  Se ~ dbeta(Se_beta_a, Se_beta_b)
+  Sp ~ dbeta(Sp_beta_a, Sp_beta_b)
+
+  ## Probability of not eliminating the infection
+  tau2 ~ dbeta(tau2_beta_a, tau2_beta_b)
+
+  ## Logistic regression coefficients
+  for(i_rf in 1:n_risk_factors){
+
+    theta[i_rf] ~ dnorm(theta_norm_mean[i_rf], theta_norm_prec[i_rf])
+
+  }
+
+}',
+      file =  "JAGS_model.txt")
+}
+
