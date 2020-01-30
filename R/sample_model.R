18082b90bbe431afbd3e9dfb9c78b236eb924b6a
Aurélien Madouasse <aurelien.madouasse@oniris-nantes.fr>
2020-01-30 18:28:42 +0100

    Model added for animal level, 1 test, risk factors

--- R/sample_model.R
+++ R/sample_model.R
@@ -375,3 +375,73 @@ sample_model.animal_1test <- function(compiled_model,
   )
 
   }
+
+
+
+#' @export
+sample_model.animal_1test_rf <- function(compiled_model,
+                                      n_burnin,
+                                      n_iter,
+                                      n_thin){
+
+  class(compiled_model) <- "jags"
+
+  ## iterations for burnin
+  update(compiled_model, n.iter = n_iter)
+
+  ## samples from posterior distributions
+  variables_to_save <- c("Se", "Sp", "pi_within", "theta", "tau2",
+                         "predicted_proba", "predicted_status")
+
+  samples <- rjags::coda.samples(compiled_model,
+                                 variable.names = variables_to_save,
+                                 n.iter = n_iter,
+                                 thin = n_thin)
+
+  ## tidying the results for predcited probabilities
+  predictions <- tidybayes::spread_draws(samples,
+                                         predicted_proba[herd_id],
+                                         predicted_status[herd_id])
+
+  ## tidying the results for model parameters
+  parameters <- tidybayes::spread_draws(samples, Se, Sp, pi_within, theta[..], tau2)
+
+  ## Percentiles of predicted probabilities of infection according to predicted status
+  choice_cutoff <- data.frame(
+    cutoff = seq(0, 1, by = .01),
+    pred_Se = rep(NA),
+    pred_Sp = rep(NA),
+    pred_ppv = rep(NA),
+    pred_npv = rep(NA)
+  )
+
+  for(i in 1:nrow(choice_cutoff)){
+
+    cutoff <- choice_cutoff$cutoff[i]
+
+    true_pos <- nrow(predictions[predictions$predicted_status == 1 &
+                                   predictions$predicted_proba > cutoff,])
+    true_neg <- nrow(predictions[predictions$predicted_status == 0 &
+                                   predictions$predicted_proba <= cutoff,])
+    false_pos <- nrow(predictions[predictions$predicted_status == 0 &
+                                    predictions$predicted_proba > cutoff,])
+    false_neg <- nrow(predictions[predictions$predicted_status == 1 &
+                                    predictions$predicted_proba <= cutoff,])
+
+    choice_cutoff$pred_Se[i] <- true_pos / (true_pos + false_neg)
+    choice_cutoff$pred_Sp[i] <- true_neg / (true_neg + false_pos)
+    choice_cutoff$pred_ppv[i] <- true_pos / (true_pos + false_pos)
+    choice_cutoff$pred_npv[i] <- true_neg / (true_neg + false_neg)
+
+  }
+
+  choice_cutoff$pred_ppv[is.nan(choice_cutoff$pred_npv)] <- choice_cutoff$pred_Se[is.nan(choice_cutoff$pred_npv)]
+  choice_cutoff$pred_ppv[is.nan(choice_cutoff$pred_ppv)] <- choice_cutoff$pred_Sp[is.nan(choice_cutoff$pred_ppv)]
+
+  list(
+    parameters = parameters,
+    proba_inf = predictions,
+    choice_cutoff = choice_cutoff
+  )
+
+}
