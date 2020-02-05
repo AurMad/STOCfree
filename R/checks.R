fdb9e978c6060f77602afb77f152ffe8bb2cee88
Aurélien Madouasse <aurelien.madouasse@oniris-nantes.fr>
2020-02-05 08:09:36 +0100

    Function to extract herd data from status id added

---
+++ R/checks.R
@@ -0,0 +1,38 @@
+#' Extract data from a STOCfree_data object based on status ids
+#'
+#' @param sfd a STOCfree_data object
+#' @param status_id status id usually returned by error message
+#'
+#' @details This function is designed to help debugging error messages when compiling the JAGS model. Error messages can refer to Status whih is the internal variable refering to a month in a herd. The function will extract herds associated with specific statuses.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+extract_herd_from_status_id <- function(sfd,
+                                        status_id = integer()){
+
+  ## herd level test data
+  herd_test <- sfd$herd_test_data
+
+  ## list of herds corresponding to the status_id provided
+  herds <- rep(NA, length(status_id))
+
+  for(i in 1:length(status_id)){
+
+    herds[i] <- herd_test$herd_id[herd_test$ind_i <= status_id[i] & herd_test$ind_p >= status_id[i]]
+
+  }
+
+  sfd$herd_id_corresp <- sfd$herd_id_corresp[sfd$herd_id_corresp$herd_id %in% herds,]
+  sfd$test_data <- sfd$test_data[sfd$test_data$herd_id %in% herds,]
+  sfd$herd_test_data <- sfd$herd_test_data[sfd$herd_test_data$herd_id %in% herds,]
+
+  herd_subset <- list(
+    herd_id_corresp = sfd$herd_id_corresp[sfd$herd_id_corresp$herd_id %in% herds,],
+    test_data = sfd$test_data[sfd$test_data$herd_id %in% herds,],
+    herd_test_data = sfd$herd_test_data[sfd$herd_test_data$herd_id %in% herds,]
+  )
+
+  herd_subset
+}
--- R/glm_nwinf_lagged.R
+++ R/glm_nwinf_lagged.R
@@ -23,7 +23,7 @@ date_from_lag <- function(date = character(), time_lag = integer()){
 }
 
 
-#' Modelling of lagged relationship between risk factor occurrence and new infection
+#' Modelling of all possible lagged relationships between risk factor occurrence and new infection
 #'
 #' @param sf_data a object of class STOCfree_data with test results
 #' @param rf_data risk factor data
@@ -47,6 +47,174 @@ glm_nwinf_lagged <- function(sf_data,
                              rf_col = character(),
                              lag1 = 0,
                              lag2 = 36){
+
+  ## Month of first test -> month_id = 1
+  month_first <- attr(sf_data, "month first test")
+  ## Month of last test
+  month_last <- attr(sf_data, "month last test")
+
+  ## List of months used in the study
+  ## month_id = 0 for the first month in the STOCfree dataset
+  rf_first_month <- date_from_lag(
+    date = paste0(month_first, "-01"), time_lag = -lag2)
+
+  all_months <- data.frame(
+    date__1 = seq(as.Date(rf_first_month),
+                  as.Date(paste0(month_last, "-01")), by = "1 month"),
+    month_id = rep(NA),
+    stringsAsFactors = FALSE
+  )
+
+  all_months$month_id <- 1:nrow(all_months) -
+    which(all_months$date__1 == paste0(month_first, "-01"))
+
+  # nwinf_data <- STOCfree_data$test_data
+  nwinf_data <- sf_data$test_data
+
+  ## renaming column with test results
+  colnames(nwinf_data)[match(sf_data$var_names[c("test_res_col", "test_date_col")],
+                             colnames(nwinf_data))] <- c("test_res", "test_date")
+
+  nwinf_data <- nwinf_data[order(nwinf_data$status_id),]
+
+  nwinf_data$prev_herd_id    <- c(NA, nwinf_data$herd_id[-nrow(nwinf_data)])
+  ## Previous month id
+  nwinf_data$prev_month_id   <- c(NA, nwinf_data$month_id[-nrow(nwinf_data)])
+  nwinf_data$prev_month_id   <- with(nwinf_data, ifelse(herd_id == prev_herd_id,
+                                                        prev_month_id, NA))
+  ## Previous test result
+  nwinf_data$prev_test_res <- c(NA, nwinf_data$test_res[-nrow(nwinf_data)])
+  nwinf_data$prev_test_res <- with(nwinf_data, ifelse(herd_id == prev_herd_id,
+                                                      prev_test_res, NA))
+
+  ## Only rows eligible for new infections are kept
+  nwinf_data <- nwinf_data[!is.na(nwinf_data$test_res) &  !is.na(nwinf_data$prev_test_res) & nwinf_data$prev_test_res == 0,]
+
+  ## New infection variable
+  nwinf_data$nwinf <- with(nwinf_data, ifelse(test_res == 1, 1, 0))
+
+  ## month_id is changed to the midpoint between previous and current test
+  nwinf_data$mid_month_id <- with(nwinf_data,
+                                  floor(prev_month_id + .5 *(month_id - prev_month_id)))
+
+  cln <- match(c("prev_month_id", "month_id", "mid_month_id"),
+               colnames(nwinf_data))
+
+  colnames(nwinf_data)[cln] <- c("month_id_test1", "month_id_test2", "month_id")
+
+  ## Herd level data for new infection
+  rf_first_last <- by(nwinf_data,
+                      list(nwinf_data$herd_id),
+                      function(x){
+                        data.frame(
+                          herd_id = unique(x$herd_id),
+                          month_first = min(x$month_id) - lag2,
+                          month_last = max(x$month_id) - lag1
+                        )
+                      })
+
+  rf_first_last  <- do.call("rbind", rf_first_last)
+
+  ## For each herd, list of months for which risk factors need to be considered
+  rf_herd_months <- apply(rf_first_last, 1, function(x){
+    data.frame(
+      herd_id = rep(x["herd_id"], as.integer(x["month_last"]) - as.integer(x["month_first"]) + 1),
+      month_id = as.integer(x["month_first"]):as.integer(x["month_last"])
+    )})
+
+  rf_herd_months <- do.call("rbind", rf_herd_months)
+
+  ## Risk factor data - herd id added
+  rf_data <- merge(sf_data$herd_id_corresp, rf_data, all.x = TRUE)
+  rf_data$herd_id <- as.integer(rf_data$herd_id)
+
+  ## month_id added
+  rf_data$date__1 <- as.Date(paste0(format(as.Date(as.character(rf_data[[rf_date_col]])),
+                                           "%Y-%m"), "-01"))
+  rf_data <- merge(all_months, rf_data, by = "date__1")
+  # rf_data <- rf_data[order(rf_data$herd_id, rf_data$month_id),]
+
+  ## data for all herds for all necessary months
+  rf_data <- merge(rf_herd_months, rf_data, all.x = TRUE)
+
+  ## Missing values replaced with 0
+  rf_data[[rf_col]][is.na(rf_data[[rf_col]])] <- 0
+
+  ## sequence of lags to study
+  sq_lag <- expand.grid(month_id = sort(unique(nwinf_data$month_id)),
+                        time_lag = lag1:lag2)
+
+  nwinf_model_data <- merge(
+    nwinf_data[, c("herd_id", "month_id", "nwinf")],
+    sq_lag)
+
+  nwinf_model_data$rf_month_id <- with(nwinf_model_data,
+                                       month_id - time_lag)
+
+  rf_data <- rf_data[, c("herd_id", "month_id", rf_col)]
+  colnames(rf_data)[2] <- "rf_month_id"
+  colnames(rf_data)[3] <- "risk_factor"
+
+  nwinf_model_data <- merge(nwinf_model_data, rf_data, all = TRUE)
+
+  ## Modelling of lagged relationships
+
+  # Grid of all possible lags to explore
+  l1l2 <- expand.grid(
+    lag1 = lag1:lag2,
+    lag2 = lag1:lag2,
+    l = rep(NA),
+    AIC = rep(NA)
+  )
+
+  l1l2 <- l1l2[l1l2$lag2 >= l1l2$lag1,]
+  l1l2$l <- with(l1l2, lag2 - lag1)
+
+  for(i in 1:nrow(l1l2)){
+
+    l1 <- l1l2$lag1[i]
+    l2 <- l1l2$lag2[i]
+
+    model_data <- dplyr::filter(nwinf_model_data,
+                                time_lag >= l1 & time_lag <= l2)
+    model_data <- dplyr::group_by(model_data,
+                                  herd_id, month_id)
+    model_data <- dplyr::summarise(model_data,
+                                   nwinf = unique(nwinf),
+                                   risk_factor = sum(risk_factor))
+
+    l1l2$AIC[i] <- AIC(glm(nwinf ~ risk_factor,
+                           data = model_data,
+                           family = binomial(link = "logit")))
+
+  }
+
+  l1l2
+
+}
+
+
+#' Modelling of lagged relationships between risk factor occurrence and new infection
+#'
+#' @param sf_data
+#' @param rf_data
+#' @param rf_herd_col
+#' @param rf_date_col
+#' @param rf_col
+#' @param lag1
+#' @param lag2
+#'
+#' @return
+#' @export
+#'
+#' @examples
+glm_nwinf <- function(sf_data,
+                      rf_data,
+                      rf_herd_col = character(),
+                      rf_date_col = character(),
+                      rf_col = character(),
+                      lag1 = 0,
+                      lag2 = 36){
 
   ## Month of first test -> month_id = 1
   month_first <- attr(sf_data, "month first test")
@@ -150,21 +318,8 @@ glm_nwinf_lagged <- function(sf_data,
 
   ## Modelling of lagged relationships
 
-  # Grid of all possible lags to explore
-  l1l2 <- expand.grid(
-    lag1 = lag1:lag2,
-    lag2 = lag1:lag2,
-    l = rep(NA),
-    AIC = rep(NA)
-  )
-
-  l1l2 <- l1l2[l1l2$lag2 >= l1l2$lag1,]
-  l1l2$l <- with(l1l2, lag2 - lag1)
-
-  for(i in 1:nrow(l1l2)){
-
-    l1 <- l1l2$lag1[i]
-    l2 <- l1l2$lag2[i]
+    l1 <- lag1
+    l2 <- lag2
 
     model_data <- dplyr::filter(nwinf_model_data,
                                 time_lag >= l1 & time_lag <= l2)
@@ -174,12 +329,10 @@ glm_nwinf_lagged <- function(sf_data,
                                    nwinf = unique(nwinf),
                                    risk_factor = sum(risk_factor))
 
-    l1l2$AIC[i] <- AIC(glm(nwinf ~ risk_factor,
+   mdl <- glm(nwinf ~ risk_factor,
                            data = model_data,
-                           family = binomial(link = "logit")))
+                           family = binomial(link = "logit"))
 
-  }
-
-  l1l2
+  mdl
 
 }
