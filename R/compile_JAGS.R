7fd273d2641f2c4b71e3ac4322907769556a9a33
Aurélien Madouasse <aurelien.madouasse@oniris-nantes.fr>
2020-03-10 10:09:58 +0100

    Animal level model allowing several tests added.

--- R/compile_JAGS.R
+++ R/compile_JAGS.R
@@ -444,7 +444,6 @@ compile_JAGS.animal_1test_rf = function(data, n_chains){
   ## test results used for prediction
   n_pos_for_pred <- data$test_data[data$test_data$month_id == month_max & !is.na(data$test_data[,"n_tested"]), "n_pos"]
   n_tested_for_pred <- data$test_data[data$test_data$month_id == month_max & !is.na(data$test_data[,"n_tested"]), "n_tested"]
-  test_id_for_pred <- data$test_data[data$test_data$month_id == month_max & !is.na(data$test_data[,"n_tested"]), "test_id"]
 
   ## Priors for risk factors
   risk_factors <- data$risk_factors[data$risk_factors$ref == 0,]
@@ -499,6 +498,94 @@ compile_JAGS.animal_1test_rf = function(data, n_chains){
   if (file.exists("JAGS_model.txt")) file.remove("JAGS_model.txt")
 
   class(JAGS_model_compiled) <- c("jags", "animal_1test_rf")
+
+  JAGS_model_compiled
+
+}
+
+#' @export
+compile_JAGS.animal_ntests_rf = function(data, n_chains){
+
+  n_herds <- attr(data, "number of herds")
+  month_max <- max(data$test_data$month_id)
+
+  ## test results
+  n_pos    <- data$test_data[data$test_data$month_id < month_max, "n_pos"]
+  n_tested <- data$test_data[data$test_data$month_id < month_max, "n_tested"]
+  test_id  <- data$test_data$test_id[data$test_data$month_id < month_max]
+  ## Indices when test performed in the expanded dataset
+  ind_test <- data$test_data$status_id[data$test_data$month_id < month_max]
+
+  ## Herds for which a test was performed on the month to predict
+  ind_last_is_test <- which(data$herd_test_data$last_is_test == 1)
+  ind_p_test <- data$herd_test_data$ind_p[ind_last_is_test]
+  n_pred_test <- length(ind_last_is_test)
+
+  ## Herds for which a test was NOT performed on the month to predict
+  ind_last_is_not_test <- which(data$herd_test_data$last_is_test == 0)
+  ind_p_no_test <- data$herd_test_data$ind_p[ind_last_is_not_test]
+  n_pred_no_test <- length(ind_last_is_not_test)
+
+  ## test results used for prediction
+  n_pos_for_pred <- data$test_data[data$test_data$month_id == month_max & !is.na(data$test_data[,"n_tested"]), "n_pos"]
+  n_tested_for_pred <- data$test_data[data$test_data$month_id == month_max & !is.na(data$test_data[,"n_tested"]), "n_tested"]
+  test_id_for_pred <- data$test_data[data$test_data$month_id == month_max & !is.na(data$test_data[,"n_tested"]), "test_id"]
+
+  ## Priors for risk factors
+  risk_factors <- data$risk_factors[data$risk_factors$ref == 0,]
+  theta_norm_mean <- risk_factors$mean_prior
+  theta_norm_prec <- 1 / risk_factors$sd_prior^2
+
+  ## Priors for test characteristics
+  test_char <- data$test_perf_prior
+
+  ## Data used by JAGS
+  JAGS_data <- list(
+    n_herds = n_herds,
+    ind_i = data$herd_test_data$ind_i,
+    ind_j = data$herd_test_data$ind_j,
+    ind_f = data$herd_test_data$ind_f,
+    ind_p = data$herd_test_data$ind_p,
+    n_tests_perf = length(ind_test),
+    ind_test = as.integer(ind_test),
+    n_pos = as.integer(n_pos),
+    n_tested = as.integer(n_tested),
+    test_id = as.integer(test_id),
+    n_pos_for_pred = n_pos_for_pred,
+    n_tested_for_pred = n_tested_for_pred,
+    test_id_for_pred = as.integer(test_id_for_pred),
+    n_pred_test = n_pred_test,
+    ind_last_is_test = ind_last_is_test,
+    n_pred_no_test = n_pred_no_test,
+    ind_p_test = ind_p_test,
+    ind_last_is_not_test = ind_last_is_not_test,
+    ind_p_no_test = ind_p_no_test,
+    pi1_beta_a = data$inf_dyn_priors["pi1_a"],
+    pi1_beta_b = data$inf_dyn_priors["pi1_b"],
+    pi_within_a = data$inf_dyn_priors["pi_within_a"],
+    pi_within_b = data$inf_dyn_priors["pi_within_b"],
+    tau2_beta_a = data$inf_dyn_priors["tau2_a"],
+    tau2_beta_b = data$inf_dyn_priors["tau2_b"],
+    Se_beta_a = test_char$Se_a,
+    Se_beta_b = test_char$Se_b,
+    Sp_beta_a = test_char$Sp_a,
+    Sp_beta_b = test_char$Sp_b,
+    theta_norm_mean = theta_norm_mean,
+    theta_norm_prec = theta_norm_prec,
+    n_risk_factors = length(theta_norm_mean),
+    risk_factors = as.matrix(data$risk_factor_data[, -(1:3)])
+  )
+
+  write_JAGS_model(data)
+
+  JAGS_model_compiled <- rjags::jags.model(
+    file = "JAGS_model.txt",
+    data = JAGS_data,
+    n.chains = n_chains)
+
+  if (file.exists("JAGS_model.txt")) file.remove("JAGS_model.txt")
+
+  class(JAGS_model_compiled) <- c("jags", "animal_ntests_rf")
 
   JAGS_model_compiled
 
--- R/write_JAGS_model.R
+++ R/write_JAGS_model.R
@@ -667,3 +667,124 @@ write_JAGS_model.animal_1test_rf <- function(data){
 }',
       file =  "JAGS_model.txt")
 }
+
+
+
+
+write_JAGS_model.animal_ntests_rf <- function(data){
+
+  cat('model{
+
+##############################################################################
+###  Inference from historical data
+##############################################################################
+
+  ## Loop for infection dynamics up to test before last
+  for(h in 1:n_herds){
+
+    # First month
+    pi[ind_i[h]] ~ dbeta(pi1_beta_a, pi1_beta_b)
+    Status[ind_i[h]] ~ dbern(pi[ind_i[h]])
+
+    # Months 2 to 1 minus final
+    for(t in ind_j[h]:ind_f[h]){
+
+    logit(tau1[t]) <- inprod(risk_factors[t,], theta)
+
+    pi[t] <- tau1[t] * (1 - Status[t-1]) +
+               tau2 * Status[t-1]
+
+    Status[t] ~ dbern(pi[t])
+
+    } #t
+
+  ## tau1 for test to predict
+  logit(tau1[ind_p[h]]) <- inprod(risk_factors[ind_p[h],], theta)
+
+  }
+
+  ## Loop for test results - historical data
+  for(i in 1:n_tests_perf){
+
+   p_infected[i] <- Status[ind_test[i]] * pi_within
+
+   p_test_pos[i] <- Se[test_id[i]] * p_infected[i] +
+                  (1 - Sp[test_id[i]]) * (1 - p_infected[i])
+
+   n_pos[i] ~ dbin(p_test_pos[i], n_tested[i])
+
+   }
+
+
+##############################################################################
+###  Prediction of probability of infection
+##############################################################################
+
+  ## Loop for statuses to predict when test result is available
+  for(j in 1:n_pred_test){
+
+    ## Indices in loops:
+    # ind_p_test: relates to status index
+    # ind_last_is_test relates to herd_id -> 1 to number of herds between this loop and the next
+    # test_for_pred relates to the cases when test is available for prediction -> 1 to number of such cases
+
+    ## Distribution of number of positive tests
+    n_pos_for_pred[j] ~ dbinom(p_pos[j], n_tested_for_pred[j])
+    p_pos[j] ~ dbeta(1, 1)
+
+    # After having observed the risk factors and the test result
+    pi[ind_p_test[j]] <- tau1[ind_p_test[j]] * (1 - Status[ind_p_test[j] - 1]) +
+                         tau2 * Status[ind_p_test[j] - 1]
+
+    predicted_proba[ind_last_is_test[j]] <-
+    p_pos[j] *
+      (Se[test_id_for_pred[j]] * pi[ind_p_test[j]] * pi_within) /
+      (Se[test_id_for_pred[j]] * pi[ind_p_test[j]] * pi_within + (1 - Sp[test_id_for_pred[j]]) * (1 - pi[ind_p_test[j]])) +
+    (1 - p_pos[j]) *
+      (1 - Se[test_id_for_pred[j]]) * pi[ind_p_test[j]]  * pi_within /
+      ((1 - Se[test_id_for_pred[j]]) * pi[ind_p_test[j]] * pi_within + Sp[test_id_for_pred[j]] * (1 - pi[ind_p_test[j]]))
+
+    predicted_status[ind_last_is_test[j]] ~ dbern(predicted_proba[ind_last_is_test[j]])
+
+  }
+
+
+  ## Loop for statuses to predict when test result is not available
+  for(k in 1:n_pred_no_test){
+
+    predicted_proba[ind_last_is_not_test[k]] <- tau1[ind_last_is_not_test[k]] * (1 - Status[ind_p_no_test[k] - 1]) +
+                                                tau2 * Status[ind_f[ind_last_is_not_test[k]]]
+
+    predicted_status[ind_last_is_not_test[k]] ~ dbern(predicted_proba[ind_last_is_not_test[k]])
+
+  }
+
+
+##############################################################################
+###  Priors
+##############################################################################
+
+  ## Priors for sensitivities and specificities
+  for(i_test in 1:n_tests){
+
+  Se[i_test] ~ dbeta(Se_beta_a[i_test], Se_beta_b[i_test])
+  Sp[i_test] ~ dbeta(Sp_beta_a[i_test], Sp_beta_b[i_test])
+
+  }
+
+  ## Prior for within herd prevalence in infected herds
+  pi_within ~ dbeta(pi_within_a, pi_within_b)
+
+  ## Probability of not eliminating the infection
+  tau2 ~ dbeta(tau2_beta_a, tau2_beta_b)
+
+  ## Logistic regression coefficients
+  for(i_rf in 1:n_risk_factors){
+
+  theta[i_rf] ~ dnorm(theta_norm_mean[i_rf], theta_norm_prec[i_rf])
+
+ }
+
+}',
+      file =  "JAGS_model.txt")
+}
