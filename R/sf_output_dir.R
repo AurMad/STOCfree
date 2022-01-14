6dd336b01cfcc7da1d4020c501b792f484360bba
Aurélien Madouasse <aurelien.madouasse@oniris-nantes.fr>
2022-01-14 11:32:53 +0100

    Second attempt at solving Windows/Unix path problems.

--- R/sf_output_dir.R
+++ R/sf_output_dir.R
@@ -1,19 +1,23 @@
 ## Folder where models are saved
 STOCfree_files <- function(out_path = out_path){
 
-  if(.Platform$OS.type == "windows") out_path <- paste0("./", out_path)
+  out_path0 <- out_path
+  out_path1 <- paste0("./", out_path)
+
+  if(.Platform$OS.type == "windows") out_path <- out_path1
   if(.Platform$OS.type == "unix") out_path <- paste0("/", out_path)
 
   ## Does the folder already exists?
   ls_dirs <- list.dirs()
 
-  if(!out_path %in% ls_dirs){
+  if(!out_path1 %in% ls_dirs){
 
     dir.create(paste0(getwd(), out_path))
 
   }
 
-  return(out_path)
+  if(.Platform$OS.type == "windows") return(out_path)
+  if(.Platform$OS.type == "unix") return(out_path0)
 
 }
 
